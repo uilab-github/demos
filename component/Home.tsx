e24ea09ba0a3b69f2abb7a9e39b3fdd6d7090d08
ByungSeoAn <quark325@gmail.com>
2021-11-18 15:40:04 +0900

    Delete unnecessary files

---
+++ component/Home.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>uilab demo</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default Home;
