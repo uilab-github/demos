f47e5eab31c3ed2b0c2fec362f462d5ab1291ae7
ByungSeoAn <quark325@gmail.com>
2021-11-21 10:01:19 +0900

    Add template to modify styles in mdx

--- component/Home.tsx
+++ component/Home.tsx
@@ -3,12 +3,17 @@ import { NextPage } from 'next';
 import HomeContent from '../post/homeContent.mdx';
 import Layout from './layout/Layout';
 import classes from './Home.module.css';
+import { editorStyle } from 'styles/editorStyle';
+import { MDXProvider } from '@mdx-js/react';
 
 const Home: NextPage = () => {
   return (
     <Layout title={'U&I Lab Demo'}>
       <div className={classes.contentContainer}>
-        <HomeContent />
+        <MDXProvider components={editorStyle}>
+          <HomeContent />
+        </MDXProvider>
+        {/* <HomeContent components={editorStyle} /> */}
       </div>
     </Layout>
   );
---
+++ styles/editorStyle.tsx
@@ -0,0 +1,6 @@
+import { MDXProviderComponentsProp } from '@mdx-js/react';
+import classes from './editorStyle.module.css';
+
+export const editorStyle: MDXProviderComponentsProp = {
+  // h1: (props) => <h1 className={classes.h1} {...props} />,
+};
