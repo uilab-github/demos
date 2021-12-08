f8beed01fd853966d4b8c9c7ffe663ea00c13c39
ByungSeoAn <quark325@gmail.com>
2021-12-08 14:38:36 +0900

    Connect map to radio tags

--- component/map/MapView.tsx
+++ component/map/MapView.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Map } from './Map';
 import classes from './MapView.module.css';
 import { NationDistribution } from 'component/chart/NationUtil';
@@ -7,44 +7,50 @@ import { BEFORE_DATA, AFTER_DATA } from 'data/paperDataLoader';
 import { RadioOption } from 'component/chart/ChartAttributeRadioTag';
 
 export const MapView = () => {
-  const beforeNationDistribution: NationDistribution = BEFORE_DATA.pirate.KO;
-  const afterNationDistribution: NationDistribution = AFTER_DATA.pirate.KO;
-
   const attributes = Object.keys(AFTER_DATA);
   const baseAttribute = attributes[0];
 
   const languages = Object.keys(AFTER_DATA[baseAttribute]);
-  const baseLangague = languages[0];
+  const baseLanguage = languages[0];
 
   const [attribute, setAttribute] = useState(baseAttribute);
-  const [language, setLanguage] = useState(baseLangague);
+  const [language, setLanguage] = useState(baseLanguage);
+  const [beforeDistribution, setBeforeDistribution] =
+    useState<NationDistribution>(BEFORE_DATA[baseAttribute][baseLanguage]);
+  const [afterDistribution, setAfterDistribution] =
+    useState<NationDistribution>(AFTER_DATA[baseAttribute][baseLanguage]);
+
+  const isActive = (geo) => {
+    return getNationFromISOA3(geo.properties.ISO_A3) !== '';
+  };
+
+  useEffect(() => {
+    setBeforeDistribution(BEFORE_DATA[attribute][language]);
+    setAfterDistribution(AFTER_DATA[attribute][language]);
+  }, [attribute, language]);
 
   return (
     <div>
       <div className={classes.map}>
         <Map
-          isActive={(geo) => {
-            return getNationFromISOA3(geo.properties.ISO_A3) !== '';
-          }}
+          isActive={isActive}
           getColorPoint={(geo) => {
             const nation = getNationFromISOA3(geo.properties.ISO_A3);
             if (nation === '') {
               return 0;
             }
-            const distribution = beforeNationDistribution[nation];
+            const distribution = beforeDistribution[nation];
             return Math.min(-1.5 / 6 + ((1.5 + 6) / 6) * distribution * 2.0, 1);
           }}
         />
         <Map
-          isActive={(geo) => {
-            return getNationFromISOA3(geo.properties.ISO_A3) !== '';
-          }}
+          isActive={isActive}
           getColorPoint={(geo) => {
             const nation = getNationFromISOA3(geo.properties.ISO_A3);
             if (nation === '') {
               return 0;
             }
-            const distribution = afterNationDistribution[nation];
+            const distribution = afterDistribution[nation];
             return Math.min(-1.5 / 6 + ((1.5 + 6) / 6) * distribution * 2.0, 1);
           }}
         />
