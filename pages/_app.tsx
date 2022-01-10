3a230ea3a29fa3ac65004c355e361b63d1c51cd7
ByungSeoAn <quark325@gmail.com>
2022-01-10 15:02:35 +0900

    Remove useless redux code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
